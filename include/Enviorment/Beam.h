35b5c4d144b90cf6da48a6789776a52339950297
crirex <crirex5k@gmail.com>
2019-04-17 04:40:55 +0300

    Refactorized the code and made the beam class work.

--- include/Enviorment/Beam.h
+++ include/Enviorment/Beam.h
@@ -25,23 +25,22 @@ public:
 
 	Beam(const std::shared_ptr<chrono::fea::ChNodeFEAxyzrot> & firstNode,
 		const std::shared_ptr<chrono::fea::ChNodeFEAxyzrot> & secondNode,
-		const std::shared_ptr<chrono::fea::ChBeamSectionAdvanced> & section);
+		const double widthY = 0.01, const double widthZ = 0.01);
 	~Beam() = default;
 
 public:
-	std::shared_ptr<chrono::fea::ChMesh> GetMesh() const;
-	const std::shared_ptr<chrono::fea::ChVisualizationFEAmesh> & GetVisualizationMesh() const;
-	const std::shared_ptr<chrono::fea::ChElementBeamEuler> & GetBeamElement() const;
-	std::shared_ptr<chrono::fea::ChNodeFEAxyzrot> GetFirstNode() const;
-	std::shared_ptr<chrono::fea::ChNodeFEAxyzrot> GetSecondNode() const;
+	std::shared_ptr<chrono::fea::ChNodeFEAxyzrot>& GetFirstNode();
+	std::shared_ptr<chrono::fea::ChNodeFEAxyzrot>& GetSecondNode();
+	std::shared_ptr<chrono::fea::ChMesh> CreateBeam();
 
 private:
 	void SetSection(double widthY, double widthZ);
-	void InitVisualizationMesh();
 
+public:
+	std::shared_ptr<chrono::fea::ChNodeFEAxyzrot> m_node1;
+	std::shared_ptr<chrono::fea::ChNodeFEAxyzrot> m_node2;
 private:
-	std::shared_ptr<chrono::fea::ChMesh> m_mesh;
-	std::shared_ptr<chrono::fea::ChElementBeamEuler> m_beamElement;
-	std::shared_ptr<chrono::fea::ChVisualizationFEAmesh> m_visualizationMesh;
+	double m_section_widthY;
+	double m_section_widthZ;
 };
 
--- source/Enviorment/main.cpp
+++ source/Enviorment/main.cpp
@@ -14,7 +14,6 @@
 #include "chrono_irrlicht/ChIrrApp.h"
 #include "../../AMMO/SSSISW-2019-AntipaPower/include/Enviorment/Beam.h"
 
-
 using namespace chrono;
 using namespace chrono::fea;
 using namespace chrono::irrlicht;
@@ -32,47 +31,15 @@ int main(int argc, char* argv[])
 	application.AddTypicalCamera(core::vector3df(0.3, 0.3, 0.3));
 	application.AddLightWithShadow(core::vector3df(1, 25, -5), core::vector3df(0, 0, 0), 35, 0.2, 35, 100, 512, video::SColorf(1, 1, 1));
 
-	auto my_mesh = std::make_shared<ChMesh>();
-	my_system.Add(my_mesh);
+	auto node1 = std::make_shared<ChNodeFEAxyzrot>(ChFrame<>(ChVector<>(0, 0, 0)));
+	auto node2 = std::make_shared<ChNodeFEAxyzrot>(ChFrame<>(ChVector<>(0.1, 0, 0)));
 
-	auto beam = std::make_shared<Beam>(ChVector<>(0, 0.1, 0.1), ChVector<>(0.1, 0.1, 0.1), std::make_pair<double,double>( 0.01, 0.01 ));
-	my_system.Add(beam->GetMesh());
-	beam->GetFirstNode()->SetFixed(true);
-	beam->GetSecondNode()->SetMass(15);
-	beam->GetSecondNode()->SetForce(chrono::VectorF(0.0f, -1.0f, 0.0f));
+	node1->SetFixed(true);
+	node2->SetMass(15);
+	node2->SetForce(chrono::VectorF(0.0f, -1.0f, 0.0f));
 
-
-
-
-	auto mvisualizebeam = std::make_shared<ChVisualizationFEAmesh>(*(my_mesh.get()));
-	mvisualizebeam->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_ELEM_BEAM_MZ);
-	mvisualizebeam->SetColorscaleMinMax(-0.4, 0.4);
-	mvisualizebeam->SetSmoothFaces(true);
-	mvisualizebeam->SetWireframe(false);
-	my_mesh->AddAsset(mvisualizebeam);
-
-	auto mnode1 = std::make_shared<ChNodeFEAxyzrot>(ChFrame<>(ChVector<>(0, 0, 0)));
-	auto mnode2 = std::make_shared<ChNodeFEAxyzrot>(ChFrame<>(ChVector<>(0.1, 0, 0)));
-
-	mnode1->SetFixed(true);
-
-	my_mesh->AddNode(mnode1);
-	my_mesh->AddNode(mnode2);
-
-	mnode2->SetMass(15);
-	mnode2->SetForce(chrono::VectorF(0.0f, -1.0f, 0.0f));
-
-
-	auto beamElement = std::make_shared<ChElementBeamEuler>();
-	my_mesh->AddElement(beamElement);
-
-
-	beamElement->SetNodes(mnode1, mnode2);
-
-	auto section = std::make_shared<fea::ChBeamSectionAdvanced>(fea::ChBeamSectionAdvanced());
-	section->SetAsRectangularSection(0.01, 0.01);
-	beamElement->SetSection(section);
-
+	Beam barna(node1, node2);
+	my_system.Add(barna.CreateBeam());
 
 	application.AssetBindAll();
 	application.AssetUpdateAll();
